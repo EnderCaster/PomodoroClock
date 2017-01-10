3c37b6297aa288c9df2f6dab7d94c0bf5a230b8a
EnderCaster <mstext0@hotmail.com>
2017-01-11 00:39:24 +0800

    beta 1

--- PomodoroTimer/Form1.Designer.cs
+++ PomodoroTimer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PomodoroTimer
 {
-    partial class Form1
+    partial class Form_main
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -29,11 +29,105 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_main));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox_timeDuration = new System.Windows.Forms.TextBox();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.label_timeDuration = new System.Windows.Forms.Label();
+            this.restTime = new System.Windows.Forms.Label();
+            this.contextMenuStrip_exit = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip_exit.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipTitle = "时间剩余：";
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip_exit;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "PomodoroClock";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDown);
+            // 
+            // textBox_timeDuration
+            // 
+            this.textBox_timeDuration.Location = new System.Drawing.Point(36, 175);
+            this.textBox_timeDuration.Name = "textBox_timeDuration";
+            this.textBox_timeDuration.Size = new System.Drawing.Size(100, 21);
+            this.textBox_timeDuration.TabIndex = 0;
+            this.textBox_timeDuration.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_timeDuration_KeyPress);
+            // 
+            // button_submit
+            // 
+            this.button_submit.Location = new System.Drawing.Point(142, 175);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(75, 23);
+            this.button_submit.TabIndex = 2;
+            this.button_submit.Text = "确认";
+            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.Click += new System.EventHandler(this.button_submit_Click);
+            // 
+            // label_timeDuration
+            // 
+            this.label_timeDuration.AutoSize = true;
+            this.label_timeDuration.Location = new System.Drawing.Point(36, 157);
+            this.label_timeDuration.Name = "label_timeDuration";
+            this.label_timeDuration.Size = new System.Drawing.Size(173, 12);
+            this.label_timeDuration.TabIndex = 3;
+            this.label_timeDuration.Text = "请输入每段工作时间的时长(分)";
+            // 
+            // restTime
+            // 
+            this.restTime.AutoSize = true;
+            this.restTime.Font = new System.Drawing.Font("宋体", 50F);
+            this.restTime.Location = new System.Drawing.Point(26, 48);
+            this.restTime.Name = "restTime";
+            this.restTime.Size = new System.Drawing.Size(199, 67);
+            this.restTime.TabIndex = 4;
+            this.restTime.Text = "00:00";
+            // 
+            // contextMenuStrip_exit
+            // 
+            this.contextMenuStrip_exit.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.contextMenuStrip_exit.Name = "contextMenuStrip_exit";
+            this.contextMenuStrip_exit.Size = new System.Drawing.Size(132, 26);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.ExitToolStripMenuItem.Text = "Stop & Exit";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // Form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(255, 226);
+            this.Controls.Add(this.restTime);
+            this.Controls.Add(this.label_timeDuration);
+            this.Controls.Add(this.button_submit);
+            this.Controls.Add(this.textBox_timeDuration);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_main";
+            this.Text = "Timer for Pomodoro Technique";
+            this.VisibleChanged += new System.EventHandler(this.Form_main_VisibleChanged);
+            this.contextMenuStrip_exit.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.TextBox textBox_timeDuration;
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.Label label_timeDuration;
+        private System.Windows.Forms.Label restTime;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip_exit;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
     }
 }
 
--- PomodoroTimer/Form1.cs
+++ PomodoroTimer/Form1.cs
@@ -1,20 +1,168 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PomodoroTimer
 {
-    public partial class Form1 : Form
+    public partial class Form_main : Form
     {
-        public Form1()
+        delegate void SetTextCallback(string text);
+        delegate void SetVisibleCallback(bool visible);
+        static int TIME_DURATION_WORK = 25 * 60 * 1000;//默认25分钟
+        static int TIME_DURATION_REST = 5 * 60 * 1000;
+        static int TIME_DURATION_NOW = 0;
+        static int REST_COUNT = 0;
+        static bool RESTING = false;//false=工作，true=休息
+        static System.Timers.Timer SENCOND_CLOCK = new System.Timers.Timer();
+        public Form_main()
         {
             InitializeComponent();
         }
+
+        private void button_submit_Click(object sender, EventArgs e)
+        {
+            textBox_timeDuration.Enabled = textBox_timeDuration.Visible = label_timeDuration.Enabled = label_timeDuration.Visible = button_submit.Enabled = button_submit.Visible = false;
+            //设置秒计时器属性
+            int tmpInt = TIME_DURATION_WORK / 1000 / 60;
+            int.TryParse(textBox_timeDuration.Text, out tmpInt);
+            if (tmpInt == 0)
+            {
+                Console.WriteLine("error");
+                Application.Exit();
+            }else
+            {
+                TIME_DURATION_WORK = tmpInt * 60 * 1000;
+            }
+            SENCOND_CLOCK.Interval = 1000;//秒表
+            SENCOND_CLOCK.Elapsed += new System.Timers.ElapsedEventHandler(timerProcess);
+            this.Hide();
+        }
+        private void timerProcess(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+            changeVisibleTime();
+            //修改TIME_DURATION_NOW，并判断是否已到期限
+            TIME_DURATION_NOW += 1000;
+            if (RESTING)
+            {
+
+                if (TIME_DURATION_NOW >= TIME_DURATION_REST)
+                {
+                    TIME_DURATION_NOW = 0;
+                    this.Visible = false;
+                    REST_COUNT++;
+                    if (REST_COUNT == 4)
+                    {
+                        TIME_DURATION_REST *= 5;
+                    }else if (TIME_DURATION_REST>4)
+                    {
+                        TIME_DURATION_REST /= 5;
+                    }
+                    RESTING = !RESTING;
+                }
+            }else
+            {
+                if (TIME_DURATION_NOW >= TIME_DURATION_WORK)
+                {
+                    TIME_DURATION_NOW = 0;
+                    this.setFormVisible(true);
+                    RESTING = !RESTING;
+                }
+            }
+        }
+        private void setFormVisible(bool b) {
+            if (this.InvokeRequired)
+            {
+                SetVisibleCallback d = new SetVisibleCallback(setFormVisible);
+                this.Invoke(d,new object[]{ b });
+            }else
+            {
+                this.Visible = b;
+                if (b)
+                {
+                    this.Activate();
+                }
+            }
+        }
+
+        private void changeVisibleTime()
+        {
+            int tmpTime,second,minute;
+            string secondS, minuteS;
+            if (RESTING)
+            {
+                tmpTime = (TIME_DURATION_REST - TIME_DURATION_NOW) / 1000;
+            }else
+            {
+                tmpTime = (TIME_DURATION_WORK - TIME_DURATION_NOW) / 1000;
+            }
+            minute = tmpTime / 60;
+            second = tmpTime % 60;
+            if (minute < 10)
+            {
+                minuteS = "0" + minute.ToString();
+            }
+            else
+            {
+                minuteS = minute.ToString();
+            }
+            if (second < 10)
+            {
+                secondS = "0" + second.ToString();
+            }
+            else
+            {
+                secondS = second.ToString();
+            }
+            threadSetText(minuteS + ":" + secondS);
+        }
+        private void threadSetText(string s)
+        {
+            if (this.restTime.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(threadSetText);
+                this.Invoke(d, new object[] { s });
+            }
+            else
+            {
+                this.restTime.Text = s;
+            }
+            notifyIcon.BalloonTipText = restTime.Text;
+        }
+
+        private void Form_main_VisibleChanged(object sender, EventArgs e)
+        {
+            if (this.Visible == true)
+            {
+                threadSetText("REST");
+                SENCOND_CLOCK.Stop();
+            }
+            else
+            {
+                SENCOND_CLOCK.Start();
+            }
+        }
+
+        private void textBox_timeDuration_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //限制为数字
+            if ((int)e.KeyChar < 48 || (int)e.KeyChar > 57 && (int)e.KeyChar == 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void notifyIcon_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button==MouseButtons.Left)
+            {
+                notifyIcon.ShowBalloonTip(1000);
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
+
 }
